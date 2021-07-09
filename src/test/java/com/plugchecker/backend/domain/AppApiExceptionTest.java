4dd8ae265eccbeb26caef82bcd457081747d4f9b
dohyeon5626 <kodohyeon71@gmail.com>
2021-07-09 11:11:55 +0900

    [TEST] add exception testcode

---
+++ src/test/java/com/plugchecker/backend/domain/AppApiExceptionTest.java
@@ -0,0 +1,50 @@
+package com.plugchecker.backend.domain;
+
+import com.plugchecker.backend.dto.request.PlugIdNameRequest;
+import com.plugchecker.backend.dto.request.PlugNameRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AppApiExceptionTest extends AppApiRequest {
+
+    @Autowired
+    private PlugRepository plugRepository;
+
+    @Test
+    @DisplayName("멀티탭 등록하기_AlreadyExistException")
+    void registPlug_AlreadyExistException() throws Exception {
+        // given
+        Plug plug = Plug.builder()
+                .name("테스트 멀티탭")
+                .build();
+        plugRepository.save(plug);
+        PlugNameRequest request = new PlugNameRequest("테스트 멀티탭");
+
+        // when
+        ResultActions resultActions = requestRegistPlug(request);
+
+        // then
+        resultActions.andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("멀티탭 변경하기_NotFoundException")
+    void changePlug_NotFoundException() throws Exception {
+        // given
+        int id = -1;
+        PlugIdNameRequest request = new PlugIdNameRequest(id, "이름이름");
+        // when
+        ResultActions resultActions = requestChangePlug(request);
+
+        // then
+        resultActions.andExpect(status().is4xxClientError())
+                .andDo(print());
+
+    }
+}
